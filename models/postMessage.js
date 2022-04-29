a14d0b9639ef961dff1a17196964e749be3f9399
unknown <hapysingh1313@gmail.com>
2022-04-29 13:51:54 +0530

    added new features.

--- models/postMessage.js
+++ models/postMessage.js
@@ -1,20 +1,19 @@
 import mongoose from 'mongoose';
 
 const postSchema = mongoose.Schema({
-    title : String,
-    message : String,
-    creator : String,
-    tags : [String],
-    selectedFile : String,
-    likeCount : {
-       type : Number,
-       default : 0,
+    title: String,
+    message: String,
+    name: String,
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+    likes: { type: [String], default: [] },
+    createdAt: {
+        type: Date,
+        default: new Date(),
     },
-    createdAt : {
-        type : Date,
-        default : new Date(),
-    },
-});
+})
 
-var PostMessage = mongoose.model('PostMessage',postSchema);
+var PostMessage = mongoose.model('PostMessage', postSchema);
+
 export default PostMessage;
